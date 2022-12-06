4335597f5ac93fdb3cd3b39dbfd21970d22e502a
coderadxniel <115766320+coderadxniel@users.noreply.github.com>
2022-12-05 23:04:27 -0800

    Add files via upload

---
+++ Harmony/src/lib/Footer.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { faGithub } from '@fortawesome/free-brands-svg-icons';
+  import Icon from 'svelte-awesome';
+</script>
+
+<footer class="w-full border-t-4 border-spotify-orange bg-black">
+  <div class="container mx-auto flex items-center justify-between px-3 py-10">
+    <div class="grid w-full grid-cols-3 items-center justify-between gap-3">
+      <div class="col-span-3 mx-auto md:col-span-1 md:mx-0">
+        <a
+          href="/"
+          class="inline-flex items-center text-xl font-semibold text-white"
+        >
+          <img
+            src="/harmony_logo.png"
+            alt="Harmony Logo"
+            class="mr-3 h-10 w-10"
+            loading="lazy"
+          /> Harmony
+        </a>
+      </div>
+      <div class="col-span-3 mx-auto md:col-span-1">
+        <p class="inline-flex items-center text-white">
+          Powered by <a
+            href="https://www.spotify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Powered by Spotify"
+          >
+            <img
+              src="/Spotify_Logo_RGB_White.png"
+              alt="Spotify Logo"
+              class="ml-2 max-w-[100px]"
+              loading="lazy"
+            />
+          </a>
+        </p>
+      </div>
+      <div class="col-span-3 mx-auto md:col-span-1 md:ml-auto md:mr-0">
+        <p class="inline-flex items-center text-white">
+          Contribute on
+          <a
+            href="https://github.com/mcmxcdev/ConcertMash"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-white"
+            title="Contribute on GitHub"
+          >
+            <Icon data={faGithub} class="m-3" scale={2} />
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ Harmony/src/lib/ImageUpload.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { registerPlugin } from 'filepond';
+  import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
+  import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+  import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+  import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+  import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+  import { afterUpdate } from 'svelte';
+  import FilePond from 'svelte-filepond';
+
+  registerPlugin(
+    FilePondPluginImageExifOrientation,
+    FilePondPluginImagePreview,
+    FilePondPluginFileEncode,
+    FilePondPluginFileValidateSize,
+    FilePondPluginFileValidateType,
+  );
+
+  export let playlistImage = '';
+
+  let pond: any;
+
+  function handleAddFile(err: any, fileItem: any) {
+    if (err) {
+      console.error(err);
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      playlistImage = fileItem.getFileEncodeBase64String();
+    }
+  }
+
+  afterUpdate(() => {
+    if (!playlistImage && pond) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      pond.removeFile();
+    }
+  });
+</script>
+
+<FilePond
+  bind:this={pond}
+  name="filepond"
+  onaddfile={handleAddFile}
+  credits={false}
+  maxFileSize="256KB"
+  acceptedFileTypes={['image/jpeg']}
+  fileValidateTypeLabelExpectedTypes="Spotify API requires .jpeg"
+/>
+
+<style global>
+  @import 'filepond/dist/filepond.css';
+  @import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+</style>
